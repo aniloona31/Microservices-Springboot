a4b83e5bfa774852bc50930d26c065bf9946340f
anirudh loona <anirudhloona26@gmail.com>
2022-09-23 09:54:38 +0530

    added h2 db

--- currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
@@ -2,10 +2,21 @@ package com.microservices.currencyexchangeservice.Entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class CurrencyExchange {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
