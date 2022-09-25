31ca8cd83d0d3723c1eac187e67399e0ff7a972a
anirudh loona <anirudhloona26@gmail.com>
2022-09-25 13:19:37 +0530

    played with circuit breaker

--- currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Controller/CircuitBreakerController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Controller/CircuitBreakerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 
@@ -15,7 +15,8 @@ public class CircuitBreakerController {
 	
 	private Logger logger = org.slf4j.LoggerFactory.getLogger(CircuitBreakerController.class);
 		
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	@CircuitBreaker(name = "default",fallbackMethod = "hardcodedResponse")
 	@GetMapping("/sample-api")
 	public String sampleApi() {
 		logger.info("retrying sample api");
