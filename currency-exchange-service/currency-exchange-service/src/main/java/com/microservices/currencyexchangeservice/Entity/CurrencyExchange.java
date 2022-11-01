afd1b70464eb7c9144c425b832505c5846c42cde
anirudh loona <anirudhloona26@gmail.com>
2022-11-01 19:35:58 +0530

    created docker images and added docker-compose

--- currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 @Entity
 public class CurrencyExchange {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name = "currency_from")
